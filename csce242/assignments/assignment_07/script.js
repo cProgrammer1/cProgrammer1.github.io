35272f7b6fc9cb39f860a4f5ab7014efb81c52e6
cProgrammer1 <chrissy.dobrowolski@gmail.com>
2024-02-04 22:14:52 -0500

    Semester Project Part 3: initial commit

---
+++ csce242/assignments/assignment_07/script.js
@@ -0,0 +1,96 @@
+/*
+    Created by Chrissy Dobrowolski 
+    Assignment 7- JavaScript, Buttons, Functions, and More 
+    CSCE 242- 001 
+    Due: 2.4.2024 
+
+*/
+
+
+/* Variables: */ 
+
+/*
+let slider = document.getElementByID("slider-id");
+let output = document.getElementByID("slider-range");
+output.innerHTML = slider.value;
+*/
+
+
+
+/*
+let slider = document.getElementByID("slider-id");
+let sliderValue = slider.value;
+output.innerHTML = slider.value;
+*/
+
+/* Functions: */ 
+
+/* - const */
+
+/* -- Changes changing-image source to another placeholder image */ 
+const changeImage = () =>
+{
+    document.getElementById("changing-image").src = "https://place-hold.it/200x200/087369";
+};
+
+/* -- Rotates image based on value of slider */
+
+/*
+
+slider.oninput = showRange ()
+{
+    output.innerHTML = this.value;
+}
+
+*/
+
+/* -- Adds star to main-col3of3 */
+
+const addStar = () =>
+{
+    const starArea = document.getElementById("star-area");
+    
+    console.log("adding star");
+
+    /* starArea.innerHTML += "<section class='star'></section>"; */
+    
+    
+    const star = document.createElement("section");
+    star.classList.add("star");
+    starArea.append(star);
+
+    star.style.background = red;
+    
+};
+
+
+/* 
+
+
+slider.oninput = rotateImage () 
+{
+    output.innerHTML = this.value;
+};
+
+*/
+
+
+/*
+const rotateImage = () =>
+{
+    sliderValue = slider.value
+    document.getElementById("rotating-image").rotate = sliderValue.value;
+};
+*/
+
+
+/* - Callers: */
+
+document.getElementById("changing-image").onclick = changeImage;
+/* document.getElementByID("slider-id").oninput = showRange; */
+
+/* document.getElementById("slider-id").oninput = rotateImage; */
+
+/* document.getElementById("starArea").addEventListener("click", addStar); */
+
+document.getElementById("starArea").onclick = addStar;
